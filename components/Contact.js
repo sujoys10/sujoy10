1c62c679aaba46f1951260c246edca8ebdf3cdab
Sujoy Saha <sujoys023@gmail.com>
2020-07-06 10:05:10 +0530

    fixed outside link

--- components/Contact.js
+++ components/Contact.js
@@ -6,17 +6,17 @@ export default function Contact(){
     return (
         <div className={styles.contact}>
             <div className={styles.icon}>
-                <a href="https://github.com/sujoys10" target="_blank">
+                <a href="https://github.com/sujoys10" target="_blank" rel="noreferrer">
                     <img src="Github.png" alt="github" />
                 </a>
             </div>
             <div className={styles.icon}>
-                <a href="https://www.linkedin.com/in/sujoy-saha-768465b5/" target="_blank">
+                <a href="https://www.linkedin.com/in/sujoy-saha-768465b5/" target="_blank" rel="noreferrer">
                     <img src="Linkedin.png" alt="linkedin" />                
                 </a>
             </div>
             <div className={styles.icon}>
-                <a href="https://twitter.com/sujoys10" target="_blank">
+                <a href="https://twitter.com/sujoys10" target="_blank" rel="noreferrer">
                     <img src="Twitter.png" alt="twitter" />                
                 </a>
             </div>
